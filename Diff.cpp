fb5d33ce35ce645077534495b3c99fed1bc6a7f2
Nikita Kumari <nikkirohilla394@gmail.com>
2025-01-28 15:30:09 +0530

    initial adding Diff.cpp

---
+++ Diff.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b,diff;
+
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+diff=a-b;
+cout<<"difference of two number is"<<" "<<a<<"-"<<b<<"="<<diff;
+return 0;
+
+}
