7b6b5a0cf06d22a1c20441048b1056bb9b399a5e
Nikita Kumari <nikkirohilla394@gmail.com>
2025-01-28 16:02:22 +0530

    added Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b,mul;
+
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+mul=a*b;
+cout<<"multiplication is"<<a<<"*"<<b<<"="<<mul;
+return 0;
+
+}
